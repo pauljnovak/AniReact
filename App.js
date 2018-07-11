ac4497c8fe4430bbecfba389a5a610b69af066b8
pauljnovak <paul.jnovak@outlook.com>
2018-07-11 21:28:48 +0200

    setup react native

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello from React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+      color: '#ffffff'
+  }
+});
