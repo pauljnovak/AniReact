220aa5e1cffa8bbd280ca0e05196a401e4dcf420
pauljnovak <paul.jnovak@outlook.com>
2018-12-08 13:17:39 +0100

    navigation with icons

--- navigation/BottomNavigation.js
+++ navigation/BottomNavigation.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import LibraryScreen from '../screens/LibraryScreen'
+import tabBarIcon from './tabBarIcon';
+
+
+class Home extends React.Component {
+    static navigationOptions = {
+      tabBarIcon: tabBarIcon('home'),
+    };
+  
+    render() {
+      return <HomeScreen />;
+    }
+  }
+  
+  class Search extends React.Component {
+    static navigationOptions = {
+      tabBarIcon: tabBarIcon('search'),
+    };
+  
+    render() {
+      return <SearchScreen />;
+    }
+  }
+  
+  class Library extends React.Component {
+    static navigationOptions = {
+      tabBarIcon: tabBarIcon('list'),
+    };
+  
+    render() {
+      return <LibraryScreen  />;
+    }
+  }
+  
 
 const BottomNavigation = createMaterialBottomTabNavigator({
-    Home: { screen: HomeScreen },
-    Search: { screen: SearchScreen },
-    Library: { screen: LibraryScreen },
+    Home,
+    Search,
+    Library,
 }, {
         initialRouteName: 'Home',
         activeColor: '#ffffff',
